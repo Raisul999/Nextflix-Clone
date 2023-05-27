e94584185c3fcf03fec95b193761a66e3d1c57c9
Raisul999 <raisulislamarnob0@gmail.com>
2023-05-27 22:19:00 +0600

    db model fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
-import Auth from "./auth";
+import { getSession, signIn, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import useCurrentuser from "../hooks/useCurrentUser";
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 export default function Home() {
+  const { data: user } = useCurrentuser();
+  console.log("user", user);
   return (
     <div>
       <h1 className="text-center text-3xl font-extrabold">Netflix clone</h1>
+      <p className="text-center">Logged in as:{user?.email}</p>
+      <button className="h-10 w-full bg-white " onClick={() => signOut()}>
+        Logout
+      </button>
     </div>
   );
 }
